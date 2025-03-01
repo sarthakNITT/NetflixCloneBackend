b87591668e5010ab4c1a40406d2e9808a8113321
sarthakKarode <sarthakkarode@gmail.com>
2025-03-02 01:01:03 +0530

    Everything working except fetching all movies

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -17,7 +17,7 @@ const protect = asyncHandler(async(req,res,next)=>{
         throw new Error("Not Authorised, token is blacklisted")
       }
       
-      const decoded = jwt.verify(token, process.env.JWT_Secret)
+      const decoded = jwt.verify(token, process.env.JWT_SECRET)
       
       req.user = await User.findById(decoded.id).select('-password')
       
